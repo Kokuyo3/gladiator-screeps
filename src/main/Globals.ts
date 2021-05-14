52d948827aaa9daf00fe773aba9b4e8bb559eea9
Kokuyo3 <6308705+Kokuyo3@users.noreply.github.com>
2021-05-14 00:48:49 -0700

    Add Towers and BodyParts to Globals

--- src/main/Globals.ts
+++ src/main/Globals.ts
@@ -1,15 +1,19 @@
-import { Creep } from "game/prototypes";
-import { Flag } from "arena/prototypes";
+import { BodyPart, Flag } from "arena/prototypes";
+import { Creep, StructureTower } from "game/prototypes";
 import { getObjectsByPrototype } from "game/utils";
 
 export class Globals {
+  public static myTower = getObjectsByPrototype(StructureTower).find(i => i.my);
+  public static enemyTower = getObjectsByPrototype(StructureTower).find(i => !i.my);
   public static myCreeps: Creep[];
   public static enemyCreeps: Creep[];
   public static enemyFlag: Flag | undefined;
+  public static bodyParts: BodyPart[];
 
   public static update(): void {
     this.myCreeps = getObjectsByPrototype(Creep).filter(i => i.my);
     this.enemyCreeps = getObjectsByPrototype(Creep).filter(i => !i.my);
     this.enemyFlag = getObjectsByPrototype(Flag).find(i => !i.my);
+    this.bodyParts = getObjectsByPrototype(BodyPart);
   }
 }
